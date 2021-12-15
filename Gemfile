d7ea797ff3732e58f285f2c909b95fa76b9dfbdd
Alvaro Lopez Hernandez <alvaro.lopezhernandez@engineering.digital.dwp.gov.uk>
2021-12-15 17:22:15 +0000

    Set GitLab user and Personal Access Token

--- Gemfile
+++ Gemfile
@@ -1,4 +1,4 @@
-ruby '2.7.3'
+ruby '2.7.5'
 source 'https://rubygems.org/' do
   gem 'kitchen-terraform', '~> 5.6'
 end
--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,21 @@ update-ca-certificates || true
 # This ensures that kitchen errors are maintained when piped through sed
 set -o pipefail
 
+if [ -n "$GITLAB_USER" ] && [ -n "$GITLAB_PAT" ]; then
+  # Set GitLab user
+  git config credential.https://gitlab.com.username ${GITLAB_USER}
+
+  # Configure small script to pass GitLab PAT from $GITLAB_PAT env var
+  # This avoids PAT being on disk, shell history, and terminal output
+  # See docs: https://git-scm.com/docs/gitcredentials#_requesting_credentials
+  GIT_ASKPASS=$(mktemp) && chmod a+rx $GIT_ASKPASS && export GIT_ASKPASS
+  cat > $GIT_ASKPASS <<'EOF'
+#!/bin/sh
+exec echo "$GITLAB_PAT"
+EOF
+
+fi
+
 if [ -z "$2" ]; then
   kitchen $1
 else
